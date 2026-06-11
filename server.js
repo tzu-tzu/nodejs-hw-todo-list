ca31fba697ecb79dd6bb485adb835c91380318c8
tzu-tzu <st930012@gmail.com>
2026-06-12 00:58:34 +0800

    調整環境設定

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ const requestListener = function (req, res) {
 
 
 const server = http.createServer(requestListener);
-server.listen(3005); 
+server.listen(process.env.PORT || 3005); 
 
 // *1.留意 port
 // *2.留意 start script
